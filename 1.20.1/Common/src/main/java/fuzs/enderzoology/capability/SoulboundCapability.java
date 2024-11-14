0061fdd59778d598b38086b5b86c2b4348ebe8ed
ichiru <101532725+ichiru5555@users.noreply.github.com>
2024-11-14 23:33:25 +0900

    test

--- 1.20.1/Common/src/main/java/fuzs/enderzoology/capability/SoulboundCapability.java
+++ 1.20.1/Common/src/main/java/fuzs/enderzoology/capability/SoulboundCapability.java
@@ -4,8 +4,10 @@ import fuzs.puzzleslib.api.capability.v2.data.CapabilityComponent;
 import net.minecraft.world.entity.player.Player;
 
 public interface SoulboundCapability extends CapabilityComponent {
-
-    void saveOnDeath(Player player);
-
-    void restoreAfterRespawn(Player player);
+    if (false){
+        
+        void saveOnDeath(Player player);
+        
+        void restoreAfterRespawn(Player player);
+    }
 }
